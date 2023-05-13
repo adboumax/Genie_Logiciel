614e034240a68ac67813a2036cd4f1af934b09af
adboumax <ad.bourrel@gmail.com>
2023-05-13 17:50:50 +0200

    Modif  Station avec nouvelle + correction test

--- projetGL2023/src/test/java/fr/ul/miage/projetGL2023/test/test_itineraire_moins_changement.java
+++ projetGL2023/src/test/java/fr/ul/miage/projetGL2023/test/test_itineraire_moins_changement.java
@@ -19,16 +19,14 @@ public class test_itineraire_moins_changement {
     @DisplayName("itineraire le moins de changement Station 1 vide")
     public void itineraireMoinsChangementStation1Vide(){
         Station x =  null;
-        Station y =  new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
+        Station y = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itiniraire_moins_changement(x, y)).isExactlyInstanceOf(IllegalArgumentException.class);
     }
     @Test
     @DisplayName("itineraire le moins changement Station 2 vide")
     public void itineraireMoinsChangementStation2Vide(){
-        Station x =  new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
+        Station x = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
         Station y =  null;
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itiniraire_moins_changement(x, y)).isExactlyInstanceOf(IllegalArgumentException.class);
@@ -45,19 +43,12 @@ public class test_itineraire_moins_changement {
     @Test
     @DisplayName("itineraire le moins de changement simple")
     public void itineraireRapideSimple(){
-        Station chatelet_les_halles =  new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(30,false,3),
-                new Liaison(30,false,1),90);
-
-        Station Montmartre = new Station("Montmartre",1,3,false,null,
-                null,90);
-        Station Louvre = new Station("Louvre",1,4,false,new Liaison(30,false,5),
-                new Liaison(30,false,3),90);
-        Station Bercy = new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station chatelet_les_halles =  new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
 
         List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Bercy,Arc_de_Triomphe);
         var ln= new Metro();
@@ -77,11 +68,13 @@ public class test_itineraire_moins_changement {
                 new Liaison(30,false,1),90);
         Station Grigny_la_grande_borne2 = new Station("Grigny-la-grande-borne",3,16,false,new Liaison(30,false,1),
                 new Liaison(30,false,1),90);
+        Station Bastille = new Station("Bastille",3,14,false,new Liaison(180,false,5), new Liaison(90,false,10),30);
+
         Station Bercy2 = new Station("Bercy",3,17,false,new Liaison(30,false,1),
                 new Liaison(30,false,1),90);
         Station Champ_de_mars = new Station("Champ de mars",3,18,false,new Liaison(30,false,1),
                 new Liaison(30,false,1),90);
-        List<Station> trajet =  Arrays.asList(Musee_grevin,Place_italie,Montmartre2,Grigny_la_grande_borne2,Bercy2,Champ_de_mars);
+        List<Station> trajet =  Arrays.asList(Musee_grevin,Place_italie,Montmartre2,Grigny_la_grande_borne2,Bastille,Bercy2,Champ_de_mars);
         var ln= new Metro();
         var result = ln.Itiniraire_moins_changement(Musee_grevin,Champ_de_mars);
         assertThat(result).isEqualTo(trajet);
@@ -90,19 +83,15 @@ public class test_itineraire_moins_changement {
     @DisplayName("itineraire le moins de changement sans correspondance autre sens")
     public void itineraireLeMoinsDeChangementSansCorrespondanceAutreSens(){
 
-        Station Musee_grevin = new Station("Musée grévin",3,13,false,null,
-                new Liaison(30,false,1),90);
-        Station Place_italie = new Station("Place d'italie",3,14,false,new Liaison(30,false,1),
-                new Liaison(30,false,1),90);
-        Station Montmartre2 = new Station("Montmartre",3,15,false,new Liaison(30,false,1),
-                new Liaison(30,false,1),90);
-        Station Grigny_la_grande_borne2 = new Station("Grigny-la-grande-borne",3,16,false,new Liaison(30,false,1),
-                new Liaison(30,false,1),90);
-        Station Bercy2 = new Station("Bercy",3,17,false,new Liaison(30,false,1),
-                new Liaison(30,false,1),90);
-        Station Champ_de_mars = new Station("Champ de mars",3,18,false,new Liaison(30,false,1),
-                new Liaison(30,false,1),90);
-        List<Station> trajet =  Arrays.asList(Musee_grevin,Place_italie,Montmartre2,Grigny_la_grande_borne2,Bercy2,Champ_de_mars);
+        Station Musee_grevin = new Station("Musée grévin",3,12,false, new Liaison(90,false,14), null,30);
+        Station Place_italie = new Station("Place d'italie",3,13,false,new Liaison(180,false,3), new Liaison(90,false,12),30);
+        Station Montmartre2 = new Station("Montmartre",3,3,false,new Liaison(250,false,10), new Liaison(180,false,13),30);
+        Station Grigny_la_grande_borne2 = new Station("Grigny-la-grande-borne",3,10,false,new Liaison(90,false,14), new Liaison(250,false,3),30);
+        Station Bastille = new Station("Bastille",3,14,false,new Liaison(180,false,5), new Liaison(90,false,10),30);
+        Station Bercy2 = new Station("Bercy",3,5,false,new Liaison(90,false,15), new Liaison(180,false,14),30);
+        Station Champ_de_mars = new Station("Champ de mars",3,15,false,null, new Liaison(90,false,5),30);
+
+        List<Station> trajet =  Arrays.asList(Musee_grevin,Place_italie,Montmartre2,Grigny_la_grande_borne2,Bastille,Bercy2,Champ_de_mars);
         var ln= new Metro();
         var result = ln.Itiniraire_moins_changement(Champ_de_mars,Musee_grevin);
         assertThat(result).isEqualTo(trajet);
@@ -110,10 +99,8 @@ public class test_itineraire_moins_changement {
     @Test
     @DisplayName("itineraire le plus rapide station arrivee et depart different")
     public void itineraireRapideStationArriveeEtDepartDiff(){
-        Station x =new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station y =new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
+        Station x =new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station y =new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itiniraire_moins_changement(x, y)).isExactlyInstanceOf(IllegalArgumentException.class);
     }
--- projetGL2023/src/test/java/fr/ul/miage/projetGL2023/test/test_itineraire_passant_par_liste_point.java
+++ projetGL2023/src/test/java/fr/ul/miage/projetGL2023/test/test_itineraire_passant_par_liste_point.java
@@ -19,12 +19,9 @@ public class test_itineraire_passant_par_liste_point {
     @DisplayName("itineraire le plus rapide Station 1 vide")
     public void itinerairePassantListePointStation1Vide(){
         Station x =  null;
-        Station y =  new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Bercy = new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station y =  new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
         List<Station> list = Arrays.asList(Arc_de_Triomphe,Bercy);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itineraire_passant_liste_point(x, y,list)).isExactlyInstanceOf(IllegalArgumentException.class);
@@ -32,13 +29,10 @@ public class test_itineraire_passant_par_liste_point {
     @Test
     @DisplayName("itineraire passant par liste de point Station 2 vide")
     public void itinerairePassantParListeDePointStation2Vide(){
-        Station x =  new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
+        Station x =  new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
         Station y =  null;
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Bercy = new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
         List<Station> list = Arrays.asList(Arc_de_Triomphe,Bercy);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itineraire_passant_liste_point(x, y,list)).isExactlyInstanceOf(IllegalArgumentException.class);
@@ -48,10 +42,8 @@ public class test_itineraire_passant_par_liste_point {
     public void itineraireRapideles2stationVide(){
         Station x =  null;
         Station y =  null;
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Bercy =new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station Arc_de_Triomphe =new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
         List<Station> list = Arrays.asList(Arc_de_Triomphe,Bercy);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itineraire_passant_liste_point(x, y,list)).isExactlyInstanceOf(IllegalArgumentException.class);
@@ -60,18 +52,13 @@ public class test_itineraire_passant_par_liste_point {
     @Test
     @DisplayName("itineraire passant par liste de point simple (point sur le chemin")
     public void itinerairePassantparListeDePointSimple(){
-        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(30,false,3),
-                new Liaison(30,false,1),90);
-        Station Montmartre = new Station("Montmartre",1,3,false,null,
-                null,90);
-        Station Louvre = new Station("Louvre",1,4,false,new Liaison(30,false,5),
-                new Liaison(30,false,3),90);
-        Station Bercy =new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+
         List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Bercy,Arc_de_Triomphe);
         var ln= new Metro();
         List<Station> list =  Arrays.asList(Montmartre);
@@ -82,68 +69,77 @@ public class test_itineraire_passant_par_liste_point {
     @Test
     @DisplayName("itineraire passant par liste de point simple 2 ( 2 points sur le chemin")
     public void itineraireListeDePointSimple2(){
-        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(30,false,3),
-                new Liaison(30,false,1),90);
-        Station Montmartre = new Station("Montmartre",1,3,false,null,
-                null,90);
-        Station Louvre = new Station("Louvre",1,4,false,new Liaison(30,false,5),
-                new Liaison(30,false,3),90);
-        Station Bercy =new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
-        List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Bercy,Arc_de_Triomphe);
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+
+        List<Station> trajet =  Arrays.asList(Arc_de_Triomphe,Bercy,Louvre,Montmartre,Pigalle,chatelet_les_halles);
         var ln= new Metro();
         List<Station> list =  Arrays.asList(Montmartre,Bercy);
-        var result = ln.Itineraire_passant_liste_point(chatelet_les_halles,Arc_de_Triomphe,list);
+        var result = ln.Itineraire_passant_liste_point(Arc_de_Triomphe,chatelet_les_halles,list);
         assertThat(result).isEqualTo(trajet);
 
     }
     @Test
     @DisplayName("itineraire passant par liste de point simple (1 point à l'opposée)")
     public void itinerairePassantParUnPointOpposée(){
-        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe =new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(30,false,3),
-                new Liaison(30,false,1),90);
-        Station Montmartre = new Station("Montmartre",1,3,false,null,
-                null,90);
-        Station Louvre = new Station("Louvre",1,4,false,new Liaison(30,false,5),
-                new Liaison(30,false,3),90);
-        Station Bercy = new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
-        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(50,false,8),
-                null,120);
-        List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Bercy,Arc_de_Triomphe);
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(60,false,8), null,30);
+        Station Invalides = new Station("Invalides",2,8,false,new Liaison(210,false,4), new Liaison(60,false,7),30);
+        Station Louvre2 = new Station("Louvre",2,4,false,new Liaison(30,false,10), new Liaison(210,false,8),30);
+        List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Louvre2,Invalides,La_Defense,Invalides,Louvre2,Louvre,Bercy,Arc_de_Triomphe);
         var ln= new Metro();
         List<Station> list =  Arrays.asList(La_Defense);
         var result = ln.Itineraire_passant_liste_point(chatelet_les_halles,Arc_de_Triomphe,list);
         assertThat(result).isEqualTo(trajet);
     }
     @Test
+    @DisplayName("itineraire passant par liste de point simple (1 point à l'opposée sens inverse)")
+    public void itinerairePassantParUnPointOpposéeSensinverse(){
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(60,false,8), null,30);
+        Station Invalides = new Station("Invalides",2,8,false,new Liaison(210,false,4), new Liaison(60,false,7),30);
+        Station Louvre2 = new Station("Louvre",2,4,false,new Liaison(30,false,10), new Liaison(210,false,8),30);
+        List<Station> trajet =  Arrays.asList(Arc_de_Triomphe,Bercy,Louvre,Louvre2,Invalides,La_Defense,Invalides,Louvre2,Louvre,Montmartre,Pigalle,chatelet_les_halles);
+        var ln= new Metro();
+        List<Station> list =  Arrays.asList(La_Defense);
+        var result = ln.Itineraire_passant_liste_point(Arc_de_Triomphe,chatelet_les_halles,list);
+        assertThat(result).isEqualTo(trajet);
+    }
+
+    @Test
     @DisplayName("itineraire passant par liste de point simple (2 points à l'opposée)")
     public void itinerairePassantPar2PointsOpposées(){
-        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(30,false,2),
-                null,20);
-        Station Arc_de_Triomphe =new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(30,false,3),
-                new Liaison(30,false,1),90);
-        Station Montmartre = new Station("Montmartre",1,3,false,null,
-                null,90);
-        Station Louvre = new Station("Louvre",1,4,false,new Liaison(30,false,5),
-                new Liaison(30,false,3),90);
-        Station Bercy = new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
-        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(50,false,8),
-                null,120);
-        Station Pantheon = new Station("Panthéon",2,12,false,null,
-                new Liaison(70,false,11),120);
-        List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Louvre,Bercy,Arc_de_Triomphe);
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+
+        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(60,false,8), null,30);
+        Station Invalides = new Station("Invalides",2,8,false,new Liaison(210,false,4), new Liaison(60,false,7),30);
+        Station Louvre2 = new Station("Louvre",2,4,false,new Liaison(30,false,10), new Liaison(210,false,8),30);
+        Station Billancourt = new Station("Billancourt",2,9,false,new Liaison(120,false,10), new Liaison(30,false,4),30);
+        Station Grigny = new Station("Grigny-la-grande-Borne",2,10,false,new Liaison(120,false,11), new Liaison(120,false,9),30);
+        Station Pantheon = new Station("Panthéon",2,11,false,null, new Liaison(120,false,10),30);
+        Station Montmartre2 = new Station("Montmartre",3,3,false,new Liaison(250,false,10), new Liaison(180,false,13),30);
+        Station Grigny_la_grande_borne2 = new Station("Grigny-la-grande-borne",3,10,false,new Liaison(90,false,14), new Liaison(250,false,3),30);
+
+        List<Station> trajet =  Arrays.asList(chatelet_les_halles,Pigalle,Montmartre,Montmartre2,Grigny_la_grande_borne2,Grigny,Pantheon,Grigny,Billancourt,Louvre2,Invalides,La_Defense,Invalides,Louvre2,Louvre,Bercy,Arc_de_Triomphe);
         var ln= new Metro();
 
         List<Station> list =  Arrays.asList(La_Defense,Pantheon);
@@ -151,16 +147,38 @@ public class test_itineraire_passant_par_liste_point {
         assertThat(result).isEqualTo(trajet);
     }
     @Test
+    @DisplayName("itineraire passant par liste de point simple (2 points à l'opposée) nsens contraire")
+    public void itinerairePassantPar2PointsOpposéesSensContraire(){
+        Station chatelet_les_halles = new Station("Chatelet les Halles",1,1,false, new Liaison(120,false,2), null,30);
+        Station Pigalle = new Station("Pigalle",1,2,false,new Liaison(120,false,3), new Liaison(120,false,1),30);
+        Station Montmartre = new Station("Montmartre",1,3,false,new Liaison(90,false,4), new Liaison(120,false,2),30);
+        Station Louvre = new Station("Louvre",1,4,false,new Liaison(150,false,5), new Liaison(90,false,3),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+
+        Station La_Defense = new Station("La Défense",2,7,false,new Liaison(60,false,8), null,30);
+        Station Invalides = new Station("Invalides",2,8,false,new Liaison(210,false,4), new Liaison(60,false,7),30);
+        Station Louvre2 = new Station("Louvre",2,4,false,new Liaison(30,false,10), new Liaison(210,false,8),30);
+        Station Billancourt = new Station("Billancourt",2,9,false,new Liaison(120,false,10), new Liaison(30,false,4),30);
+        Station Grigny = new Station("Grigny-la-grande-Borne",2,10,false,new Liaison(120,false,11), new Liaison(120,false,9),30);
+        Station Pantheon = new Station("Panthéon",2,11,false,null, new Liaison(120,false,10),30);
+        Station Montmartre2 = new Station("Montmartre",3,3,false,new Liaison(250,false,10), new Liaison(180,false,13),30);
+        Station Grigny_la_grande_borne2 = new Station("Grigny-la-grande-borne",3,10,false,new Liaison(90,false,14), new Liaison(250,false,3),30);
+
+        List<Station> trajet =  Arrays.asList(Arc_de_Triomphe,Bercy,Louvre,Louvre2,Invalides,Louvre2,Billancourt,Grigny,Pantheon,Grigny,Grigny_la_grande_borne2,Montmartre2,Montmartre,Pigalle,chatelet_les_halles);
+        var ln= new Metro();
+
+        List<Station> list =  Arrays.asList(Pantheon,La_Defense);
+        var result = ln.Itineraire_passant_liste_point(chatelet_les_halles,Arc_de_Triomphe,list);
+        assertThat(result).isEqualTo(trajet);
+    }
+    @Test
     @DisplayName("itineraire le plus rapide station arrivee et depart different")
     public void itineraireRapideStationArriveeEtDepartDiff(){
-        Station x =  new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station y =new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null,
-                new Liaison(30,false,5),40);
-        Station Bercy =new Station("Bercy",1,5,false,new Liaison(50,false,6),
-                new Liaison(70,false,4),120);
+        Station x =  new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station y =new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Arc_de_Triomphe = new Station("Arc de triomphe",1,6,false,null, new Liaison(120,false,5),30);
+        Station Bercy = new Station("Bercy",1,5,false,new Liaison(120,false,6), new Liaison(150,false,4),30);
         List<Station> list = Arrays.asList(Arc_de_Triomphe,Bercy);
         var trajet= new Metro();
         assertThatThrownBy(()-> trajet.Itineraire_passant_liste_point(x, y,list)).isExactlyInstanceOf(IllegalArgumentException.class);
